a41eae4be4d9b2b2ba7c7cb935ba56b2e1b57d5e
Tom Warner <tomwamt@gmail.com>
2025-12-02 00:41:20 -0700

    Faster Day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,4 +1,6 @@
+use std::collections::HashSet;
 use std::iter::once;
+use std::num::IntErrorKind;
 
 pub struct Solution;
 
@@ -16,10 +18,71 @@ fn solve(input: &str, chunks_counts: &(impl Iterator<Item = usize> + Clone)) -> 
     let ranges = parse_ranges(input);
     ranges
         .iter()
-        .flat_map(|r| r.0..=r.1)
-        .filter(|&n| is_invalid(n, chunks_counts.clone()))
+        .flat_map(|r| generate_invalid(r.0, r.1, chunks_counts.clone()))
         .sum::<u64>()
         .to_string()
+}
+
+fn generate_invalid(
+    start: u64,
+    end: u64,
+    chunks_counts: impl Iterator<Item = usize>,
+) -> HashSet<u64> {
+    let mut invalids = HashSet::new();
+    let start_chars = start.to_string().chars().collect::<Vec<_>>();
+    let end_chars = end.to_string().chars().collect::<Vec<_>>();
+
+    for chunks in chunks_counts {
+        if chunks > end_chars.len() {
+            break;
+        }
+        log::info!("checking range {start}-{end} for invalids with {chunks} chunks");
+        let mut chunk = start_chars[0..(start_chars.len() / chunks)].to_vec();
+        if chunk.is_empty() {
+            chunk.push('1');
+        }
+        loop {
+            let chunk_len = chunk.len();
+            let candidate = match chunk
+                .iter()
+                .cycle()
+                .take(chunks * chunk_len)
+                .collect::<String>()
+                .parse::<u64>()
+            {
+                Ok(n) => n,
+                Err(e) if matches!(e.kind(), IntErrorKind::PosOverflow) => break,
+                Err(e) => panic!("Unexpected parse error for candidate ID: {e:?}"),
+            };
+
+            if candidate > end {
+                break;
+            }
+
+            log::debug!("Checking candidate invalid ID: {candidate}");
+
+            increment_chunk(&mut chunk);
+
+            if candidate < start {
+                continue;
+            }
+            invalids.insert(candidate);
+            log::debug!("Found invalid ID: {candidate}");
+        }
+    }
+    invalids
+}
+
+fn increment_chunk(chunk: &mut Vec<char>) {
+    for i in (0..chunk.len()).rev() {
+        if chunk[i] == '9' {
+            chunk[i] = '0';
+        } else {
+            chunk[i] = ((chunk[i] as u8) + 1) as char;
+            return;
+        }
+    }
+    chunk.insert(0, '1');
 }
 
 fn parse_ranges(input: &str) -> Vec<(u64, u64)> {
@@ -33,27 +96,4 @@ fn parse_ranges(input: &str) -> Vec<(u64, u64)> {
             (start, end)
         })
         .collect()
-}
-
-fn is_invalid(n: u64, chunks_counts: impl Iterator<Item = usize>) -> bool {
-    let ns: Vec<_> = n.to_string().chars().collect();
-    for chunks in chunks_counts {
-        if chunks > ns.len() {
-            break;
-        }
-        if is_repeated_chunks(&ns, chunks) {
-            return true;
-        }
-    }
-    false
-}
-
-fn is_repeated_chunks(id_vec: &[char], chunks: usize) -> bool {
-    if !id_vec.len().is_multiple_of(chunks) {
-        return false;
-    }
-    id_vec
-        .chunks(id_vec.len() / chunks)
-        .reduce(|c1, c2| if c1 == c2 { c1 } else { &[] })
-        .is_some_and(|c| !c.is_empty())
 }
